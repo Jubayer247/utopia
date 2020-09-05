66a6cc70dfa380c5c4653f0938fce9abe142954a
Md. Jubayer <mdjubayer247@gmail.com>
2020-09-05 07:37:09 +0600

    utopia105

--- app/src/main/java/xzy/benot/utopia/MainActivity2.java
+++ app/src/main/java/xzy/benot/utopia/MainActivity2.java
@@ -39,7 +39,7 @@ public class MainActivity2 extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_find_a_doctor, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
